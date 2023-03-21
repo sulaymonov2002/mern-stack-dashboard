2938a596f6a2e83278d9b633cf299c30d7303a50
sulaymonov2002 <ssamir.web2002@gmail.com>
2023-03-21 23:32:28 +0500

    feat: add new backends codes

--- server/index.js
+++ server/index.js
@@ -4,3 +4,22 @@ import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
 import helmet from "helmet";
+import morgan from "morgan";
+import clientRoutes from "./routes/client.js";
+
+/* CONFIGURATION */
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+/* ROUTES */
+app.use("/client", clientRoutes);
+app.use("/general", generalRoutes);
+app.use("/management", management);
+app.use("/sales", salesRoutes);

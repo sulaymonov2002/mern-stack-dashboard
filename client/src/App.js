3f60990dc43fb63e8e0b7864a235b423673d2d47
sulaymonov2002 <ssamir.web2002@gmail.com>
2023-03-22 11:50:15 +0500

    feat: add new App.js codes

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,19 @@
+import { useMemo } from "react";
+import { themeSettings } from "theme";
+import { useSelector } from "react-redux";
+import { createTheme } from "@mui/material";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+
 function App() {
-  return <div className="app"></div>;
+  const mode = useSelector((state) => state.global.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  return (
+    <div className="app">
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+      </ThemeProvider>
+    </div>
+  );
 }
 
 export default App;

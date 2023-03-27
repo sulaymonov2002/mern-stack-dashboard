0a755ae0ca698a3171cb8cb72aca267a84c36b1d
sulaymonov2002 <ssamir.web2002@gmail.com>
2023-03-27 09:48:11 +0500

    feat: add new codes for components Sidebar.jsx file and some codes

---
+++ client/src/components/Sidebar.jsx
@@ -0,0 +1,44 @@
+import {
+  Box,
+  Divider,
+  Drawer,
+  IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Typography,
+  useTheme,
+} from "@mui/material";
+import {
+  SettingsOutlined,
+  ChevronLeft,
+  ChevronRightOutlined,
+  HomeOutlined,
+  ShoppingCartOutlined,
+  Group2Outlined,
+  PublicOutlined,
+  PointOfSaleOutlined,
+  TodayOutlined,
+  CalendarMonthOutlined,
+  AdminPanelSettingsOutlined,
+  TrendingUpOutlined,
+  PieChartOutlined,
+} from "@mui/icons-material";
+
+import { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import FlexBetween from "./FlexBetween";
+import profileImage from "assets/profile.jpeg";
+
+const Sidebar = ({
+  drawerWidth,
+  isSidebarOpen,
+  setIsSidebarOpen,
+  isNonMobile,
+}) => {
+  return <div>Sidebar</div>;
+};
+
+export default Sidebar;

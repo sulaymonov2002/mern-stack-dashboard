19af2752245062b14f177bec49bf5a54750ca3e3
sulaymonov2002 <ssamir.web2002@gmail.com>
2023-03-31 14:35:05 +0500

    feat: add new codes for Navbar

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -17,6 +17,8 @@ import {
   ChevronRightOutlined,
   HomeOutlined,
   ShoppingCartOutlined,
+  Groups2Outlined,
+  ReceiptLongOutlined,
   PublicOutlined,
   PointOfSaleOutlined,
   TodayOutlined,
@@ -24,8 +26,6 @@ import {
   AdminPanelSettingsOutlined,
   TrendingUpOutlined,
   PieChartOutlined,
-  ReceiptLongOutlined,
-  GroupOutlined,
 } from "@mui/icons-material";
 
 import { useEffect, useState } from "react";
@@ -48,7 +48,7 @@ const navItems = [
   },
   {
     text: "Customers",
-    icon: <GroupOutlined />,
+    icon: <Groups2Outlined />,
   },
   {
     text: "Transactions",
@@ -93,6 +93,7 @@ const navItems = [
 ];
 
 const Sidebar = ({
+  user,
   drawerWidth,
   isSidebarOpen,
   setIsSidebarOpen,
@@ -103,7 +104,6 @@ const Sidebar = ({
   const navigate = useNavigate();
   const theme = useTheme();
 
-  // functional codes =>
   useEffect(() => {
     setActive(pathname.substring(1));
   }, [pathname]);
@@ -118,17 +118,17 @@ const Sidebar = ({
           anchor="left"
           sx={{
             width: drawerWidth,
-            "& MuiDrawer-paper": {
+            "& .MuiDrawer-paper": {
               color: theme.palette.secondary[200],
               backgroundColor: theme.palette.background.alt,
-              boxSizing: "border-box",
+              boxSixing: "border-box",
               borderWidth: isNonMobile ? 0 : "2px",
               width: drawerWidth,
             },
           }}
         >
-          <Box width="100px">
-            <Box m="1.5rem 2rem 3rem">
+          <Box width="100%">
+            <Box m="1.5rem 2rem 2rem 3rem">
               <FlexBetween color={theme.palette.secondary.main}>
                 <Box display="flex" alignItems="center" gap="0.5rem">
                   <Typography variant="h4" fontWeight="bold">
@@ -151,7 +151,7 @@ const Sidebar = ({
                     </Typography>
                   );
                 }
-                const lcText = text.toLocaleLowerCase();
+                const lcText = text.toLowerCase();
 
                 return (
                   <ListItem key={text} disablePadding>
@@ -160,6 +160,16 @@ const Sidebar = ({
                         navigate(`/${lcText}`);
                         setActive(lcText);
                       }}
+                      sx={{
+                        backgroundColor:
+                          active === lcText
+                            ? theme.palette.secondary[300]
+                            : "transparent",
+                        color:
+                          active === lcText
+                            ? theme.palette.primary[600]
+                            : theme.palette.secondary[100],
+                      }}
                     >
                       <ListItemIcon
                         sx={{
@@ -167,15 +177,57 @@ const Sidebar = ({
                           color:
                             active === lcText
                               ? theme.palette.primary[600]
-                              : theme,
+                              : theme.palette.secondary[200],
                         }}
-                      ></ListItemIcon>
+                      >
+                        {icon}
+                      </ListItemIcon>
+                      <ListItemText primary={text} />
+                      {active === lcText && (
+                        <ChevronRightOutlined sx={{ ml: "auto" }} />
+                      )}
                     </ListItemButton>
                   </ListItem>
                 );
               })}
             </List>
           </Box>
+          {/* 
+          <Box position="absolute" bottom="2rem">
+            <Divider />
+            <FlexBetween textTransform="none" gap="1rem" m="1.5rem 2rem 0 3rem">
+              <Box
+                component="img"
+                alt="profile"
+                src={profileImage}
+                height="40px"
+                width="40px"
+                borderRadius="50%"
+                sx={{ objectFit: "cover" }}
+              />
+              <Box textAlign="left">
+                <Typography
+                  fontWeight="bold"
+                  fontSize="0.9rem"
+                  sx={{ color: theme.palette.secondary[100] }}
+                >
+                  {user.name}
+                </Typography>
+                <Typography
+                  fontSize="0.8rem"
+                  sx={{ color: theme.palette.secondary[200] }}
+                >
+                  {user.occupation}
+                </Typography>
+              </Box>
+              <SettingsOutlined
+                sx={{
+                  color: theme.palette.secondary[300],
+                  fontSize: "25px ",
+                }}
+              />
+            </FlexBetween>
+          </Box> */}
         </Drawer>
       )}
     </Box>

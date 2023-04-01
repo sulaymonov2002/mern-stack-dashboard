c8707b4d070a1b1804eb384856d7ce1689353051
sulaymonov2002 <ssamir.web2002@gmail.com>
2023-04-01 05:22:40 +0500

    feat: add new codes for server side

---
+++ server/models/User.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    min: 2,
+    max: 100,
+  },
+  email: {
+    type: String,
+    required: true,
+    min: 50,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    min: 5,
+    },
+  cit: {
+    
+  }
+});

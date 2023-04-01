60d5af1dc0bfbea4bed2d2096c0a7103c0a34d7b
sulaymonov2002 <ssamir.web2002@gmail.com>
2023-04-01 05:49:02 +0500

    feat: add new codes for server side in User.js file

--- server/models/User.js
+++ server/models/User.js
@@ -1,27 +1,35 @@
 import mongoose from "mongoose";
 
-const UserSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    min: 2,
-    max: 100,
+const UserSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      min: 2,
+      max: 100,
+    },
+    email: {
+      type: String,
+      required: true,
+      min: 50,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 5,
+    },
+    cit: String,
+    state: String,
+    country: String,
+    occupation: String,
+    phoneNumber: String,
+    transactions: Array,
+    role: {
+      type: String,
+      enum: ["user", "admin", "superadmin"],
+      default: "admin",
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    min: 50,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-    min: 5,
-  },
-  cit: String,
-  state: String,
-  country: String,
-  occupation: String,
-  phoneNumber: String,
-  transactions: Array,
-});
+  { timestamps: true }
+);

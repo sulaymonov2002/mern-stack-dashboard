509e9073ae793cab8519ed8c25b40de9370c8004
sulaymonov2002 <ssamir.web2002@gmail.com>
2023-05-01 12:32:51 +0500

    fix: problem in client side

--- client/src/scenes/layout/index.jsx
+++ client/src/scenes/layout/index.jsx
@@ -22,7 +22,7 @@ const Layout = () => {
         isSidebarOpen={isSidebarOpen}
         setIsSidebarOpen={setIsSidebarOpen}
       />
-      <Box flexGrow={1}>
+      <Box flexGrow={1}> 
         <Navbar
           user={data || {}}
           isSidebarOpen={isSidebarOpen}
---
+++ server/controllers/client.js
@@ -0,0 +1,21 @@
+import Product from "../models/Product.js";
+import ProductStat from "../models/ProductStat";
+
+export const getProducts = async (req, res) => {
+  try {
+    const products = await Product.find();
+    const productsWithStats = await Promise.all(
+      products.map(async (product) => {
+        const stat = await ProductStat.find({
+          productId: product._id,
+        });
+        return {
+          ...product._doc,
+          stat,
+        };
+      })
+    );
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};

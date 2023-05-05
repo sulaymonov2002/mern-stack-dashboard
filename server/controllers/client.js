bed3eda9d9f1b039e99bcb69651294179a0b6001
sulaymonov2002 <ssamir.web2002@gmail.com>
2023-05-05 12:20:15 +0500

    feat: add new codes for client side server side

--- server/controllers/client.js
+++ server/controllers/client.js
@@ -1,9 +1,10 @@
 import Product from "../models/Product.js";
-import ProductStat from "../models/ProductStat";
+import ProductStat from "../models/ProductStat.js";
 
 export const getProducts = async (req, res) => {
   try {
     const products = await Product.find();
+
     const productsWithStats = await Promise.all(
       products.map(async (product) => {
         const stat = await ProductStat.find({
@@ -15,6 +16,7 @@ export const getProducts = async (req, res) => {
         };
       })
     );
+    res.status(200).json(productsWithStats);
   } catch (error) {
     res.status(404).json({ message: error.message });
   }

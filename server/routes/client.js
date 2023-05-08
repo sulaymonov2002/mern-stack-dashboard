81d8be8009a2a1ef94d6fcc8a7fe93e3e61c2a6d
sulaymonov2002 <ssamir.web2002@gmail.com>
2023-05-08 14:09:25 +0500

    feat: add new codes for server codes

--- server/routes/client.js
+++ server/routes/client.js
@@ -4,5 +4,7 @@ import { getProducts } from "../controllers/client.js";
 const router = express.Router();
 
 router.get("/products", getProducts);
+router.get("/customers", getCustomers);
+
 
 export default router;

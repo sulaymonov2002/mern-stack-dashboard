25b907f030d89fe16b86dc4197fc0d1289fe700c
sulaymonov2002 <ssamir.web2002@gmail.com>
2023-05-17 01:10:02 +0500

    feat: add new codes for server codes

---
+++ client/src/components/DataGridCustomToolbar.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Search } from "@mui/icons-material";
+import { IconButton, TextField, InputAdornment } from "@mui/material";
+
+const DataGridCustomToolbar = () => {
+  return <div>DataGridCustomToolbar</div>;
+};
+
+export default DataGridCustomToolbar;
--- client/src/scenes/transactions/index.jsx
+++ client/src/scenes/transactions/index.jsx
@@ -3,6 +3,7 @@ import { Box, useTheme } from "@mui/material";
 import { DataGrid } from "@mui/x-data-grid";
 import { useGetTransactionsQuery } from "state/api";
 import Header from "components/Header";
+import DataGridCustomToolbar from "components/DataGridCustomToolbar";
 
 const Transaction = () => {
   const theme = useTheme();
@@ -44,17 +45,61 @@ const Transaction = () => {
     },
 
     {
-      field: "occupation",
-      headerName: "Occupation",
+      field: "cost",
+      headerName: "cost",
       flex: 1,
-    },
-    {
-      field: "role",
-      headerName: "Role",
-      flex: 0.5,
+      renderCell: (params) => `$${Number(params.value)}`,
     },
   ];
-  return <div>Transaction</div>;
+  return (
+    <Box m="1.5rem 2.5rem">
+      <Header title="TRANSACTIONS" subtitle="Entire list of transactions" />
+      <Box
+        height="80vh"
+        sx={{
+          "& .MuiDataGrid-root": {
+            border: "none",
+          },
+          "& .MuiDataGrid-cell": {
+            borderBottom: "none",
+          },
+          "& .MuiDataGrid-columnHeaders": {
+            backgroundColor: theme.palette.background.alt,
+            color: theme.palette.secondary[100],
+            borderBottom: "none",
+          },
+          "& .MuiDataGrid-virtualScroller": {
+            backgroundColor: theme.palette.primary.light,
+          },
+          "& .MuiDataGrid-footerContainer": {
+            backgroundColor: theme.palette.primary.alt,
+            color: theme.palette.secondary[100],
+            borderBottom: "none",
+          },
+          "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
+            color: `${theme.palette.secondary[200]} !important`,
+          },
+        }}
+      >
+        <DataGrid
+          loading={isLoading || !data}
+          getRowId={(row) => row._id}
+          rows={(data && data.transactions) || []}
+          columns={columns}
+          rowCount={(data && data.total) || 0}
+          pagination
+          page={page}
+          pageSize={pageSize}
+          paginationMode="server"
+          sortingMode="server"
+          onPageChange={(newPage) => setPage(newPage)}
+          onPageSizeChange={(newPageSize) => setPageSize(newPageSize)}
+          onSortModelChange={(newSortModel) => setSort(...newSortModel)}
+          components={{ Toolbar: DataGridCustomToolbar }}
+        />
+      </Box>
+    </Box>
+  );
 };
 
 export default Transaction;

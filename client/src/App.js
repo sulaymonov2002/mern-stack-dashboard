312f9a0f0a30b4269933d353f03e821185738c7c
sulaymonov2002 <ssamir.web2002@gmail.com>
2023-07-25 01:03:46 +0500

    feat: add new daily codes

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import Products from "scenes/products";
 import Customers from "scenes/customers";
 import Transactions from "scenes/transactions";
 import Geography from "scenes/geography";
-
+import Overview from "scenes/overview";
 
 import { useSelector } from "react-redux";
 import { createTheme } from "@mui/material/styles";
@@ -29,6 +29,7 @@ function App() {
               <Route path="/customers" element={<Customers />} />
               <Route path="/transactions" element={<Transactions />} />
               <Route path="/geography" element={<Geography />} />
+              <Route path="/overview" element={<Overview />} />
             </Route>
           </Routes>
         </ThemeProvider>
---
+++ client/src/scenes/daily/index.jsx
@@ -0,0 +1,11 @@
+import React, { useMemo, useState } from "react";
+import { FormControl, MenuItem, InputLabel, Box, Select } from "@mui/material";
+import Header from "components/Header";
+import OverviewChart from "components/OverviewChart";
+import { useGetSalesQuery } from "state/api";
+
+const Daily = () => {
+  return <div>Daily</div>;
+};
+
+export default Daily;

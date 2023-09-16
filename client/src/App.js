3247324ef4b41a8934967020ca220f3e4de694c9
sulaymonov2002 <ssamir.web2002@gmail.com>
2023-09-16 16:49:02 +0500

    feat: add new daily codes

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,8 @@ import Customers from "scenes/customers";
 import Transactions from "scenes/transactions";
 import Geography from "scenes/geography";
 import Overview from "scenes/overview";
+import Daily from "scenes/daily";
+import Monthly from "scenes/monthly";
 
 import { useSelector } from "react-redux";
 import { createTheme } from "@mui/material/styles";
@@ -30,6 +32,8 @@ function App() {
               <Route path="/transactions" element={<Transactions />} />
               <Route path="/geography" element={<Geography />} />
               <Route path="/overview" element={<Overview />} />
+              <Route path="/daily" element={<Daily />} />
+              <Route path="/monthly" element={<Monthly />} />
             </Route>
           </Routes>
         </ThemeProvider>
